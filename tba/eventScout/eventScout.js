d3282a8419f921245a910032ab22d3e5cfdcc0d9
Richard O'Donnell <rodonnel@akamai.com>
2019-03-30 21:22:27 -0400

    Fixed bug for in-progress events

--- tba/eventScout/eventScout.js
+++ tba/eventScout/eventScout.js
@@ -10,43 +10,46 @@
     summarizeMatches2019: function(matches) {
       let summary = [];
       for (let i = 0; i < matches.length; i++) {
-        // go through blue and red alliances
-        for (let alliance of ['blue', 'red']) {
-          // console.log(alliance);
-          let tmPos = 0;
-          for (let team of matches[i].alliances[alliance].team_keys) {
-            // console.log(team);
-            let tmSum = summary.find(a => a.team === team);
-            if (typeof tmSum === 'undefined') {
-              tmSum = { team, start: _.clone(nullStats.start), climb: _.clone(nullStats.climb) };
-              summary.push(tmSum);
-            }
-            // add start position
-            const preMatchPos = matches[i].score_breakdown[alliance][`preMatchLevelRobot${++tmPos}`];
-            const preMatchLvl = preMatchPos.substr('HabLevel'.length);
-            const sandstormAction = matches[i].score_breakdown[alliance][`habLineRobot${tmPos}`];
-            if (preMatchPos === 'None' || sandstormAction !== 'CrossedHabLineInSandstorm') {
-              tmSum.start.none++;
-            } else if (preMatchLvl === '1') {
-              tmSum.start.one++;
-            } else if (preMatchLvl === '2') {
-              tmSum.start.two++;
-            } else {
-              console.warn(`EVENTSCOUT: Unknown starting location ${preMatchPos} for ${team} in match ${matches[i].key}`);
-            }
-            // add endgame
-            const endMatchPos = matches[i].score_breakdown[alliance][`endgameRobot${tmPos}`];
-            const endMatchLvl = endMatchPos.substr('HabLevel'.length);
-            if (endMatchPos === 'None') {
-              tmSum.climb.none++;
-            } else if (endMatchLvl === '1') {
-              tmSum.climb.one++;
-            } else if (endMatchLvl === '2') {
-              tmSum.climb.two++;
-            } else if (endMatchLvl === '3') {
-              tmSum.climb.three++;
-            } else {
-              console.warn(`EVENTSCOUT: Unknown climbing location ${endMatchPos} for ${team} in match ${matches[i].key}`);
+        // only go through all this if the match has data
+        if (!!matches[i].score_breakdown) {
+          // go through blue and red alliances
+          for (let alliance of ['blue', 'red']) {
+            // console.log(alliance);
+            let tmPos = 0;
+            for (let team of matches[i].alliances[alliance].team_keys) {
+              // console.log(team);
+              let tmSum = summary.find(a => a.team === team);
+              if (typeof tmSum === 'undefined') {
+                tmSum = {team, start: _.clone(nullStats.start), climb: _.clone(nullStats.climb)};
+                summary.push(tmSum);
+              }
+              // add start position
+              const preMatchPos = matches[i].score_breakdown[alliance][`preMatchLevelRobot${++tmPos}`];
+              const preMatchLvl = preMatchPos.substr('HabLevel'.length);
+              const sandstormAction = matches[i].score_breakdown[alliance][`habLineRobot${tmPos}`];
+              if (preMatchPos === 'None' || sandstormAction !== 'CrossedHabLineInSandstorm') {
+                tmSum.start.none++;
+              } else if (preMatchLvl === '1') {
+                tmSum.start.one++;
+              } else if (preMatchLvl === '2') {
+                tmSum.start.two++;
+              } else {
+                console.warn(`EVENTSCOUT: Unknown starting location ${preMatchPos} for ${team} in match ${matches[i].key}`);
+              }
+              // add endgame
+              const endMatchPos = matches[i].score_breakdown[alliance][`endgameRobot${tmPos}`];
+              const endMatchLvl = endMatchPos.substr('HabLevel'.length);
+              if (endMatchPos === 'None') {
+                tmSum.climb.none++;
+              } else if (endMatchLvl === '1') {
+                tmSum.climb.one++;
+              } else if (endMatchLvl === '2') {
+                tmSum.climb.two++;
+              } else if (endMatchLvl === '3') {
+                tmSum.climb.three++;
+              } else {
+                console.warn(`EVENTSCOUT: Unknown climbing location ${endMatchPos} for ${team} in match ${matches[i].key}`);
+              }
             }
           }
         }
